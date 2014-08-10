5431e37c158112dcb08c9b49a779e9ea3ec69b97
IlBedussa <Davidbedussa@gmail.com>
2014-08-10 16:13:11 +0300

    replaced email with names

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,5 @@ class Pin < ActiveRecord::Base
 	validates :image, presence: true
 	validates :description, presence: true
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+	
 end

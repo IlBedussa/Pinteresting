6b8524b94309b2aec78ae04bf544f5813e543955
Sahil Dhankhar <sahil1345@gmail.com>
2014-08-27 09:25:19 +0530

    Refactor + capturing twitter handle

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,22 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   validates :name, presence: true
-
-  has_many :pins
+  has_many :identities, :dependent => :destroy
+  has_many :pins, :dependent => :destroy
   
   def self.find_for_oauth(auth, signed_in_resource = nil)
     identity = Identity.find_for_oauth(auth)
     user = signed_in_resource ? signed_in_resource : identity.user
     if user.nil?
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email || auth.provider == "google_oauth2")
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
       email = auth.info.email if email_is_verified
       user = User.where(:email => email).first if email
       if user.nil?
-        user = User.new(
-          name: auth.extra.raw_info.try(:name),
-          #username: auth.info.nickname || auth.uid,
-          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
-          password: Devise.friendly_token[0,20]
-        )
+        if auth.provider == 'twitter'
+          user = self.generate_user_from_twitter(auth, email) 
+        elsif auth.provider == 'facebook'
+          user = self.generate_user_from_facebook(auth, email)  
+        end
         user.save!
       end
     end
@@ -33,5 +32,23 @@ class User < ActiveRecord::Base
     end
     user
   end
+  
+  def self.generate_user_from_twitter(auth, email)
+      user = User.new(name: auth.info.name,
+        twitter_handle: auth.info.nickname,
+        email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+        password: Devise.friendly_token[0,20]
+      )
+  end
 
+  def self.generate_user_from_facebook(auth, email)
+      user = User.new(name: auth.info.name,
+        email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+        password: Devise.friendly_token[0,20]
+      )
+  end
+  
+  def twitter_handle_display
+    twitter_handle ? "@#{twitter_handle}" : ""
+  end  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826155655) do
+ActiveRecord::Schema.define(version: 20140827030525) do
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140826155655) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "twitter_handle"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

dce69a188a0987b264a1b4ce977fe135db9ec686
Sahil Dhankhar <sahil1345@gmail.com>
2014-08-29 19:13:04 +0530

    Email fix

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,10 +2,9 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-
   def index
-  @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
- end
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+  end
 
   def show
   end
@@ -21,10 +20,7 @@ class PinsController < ApplicationController
     @pin = current_user.pins.build(pin_params)
     if @pin.save
       @user = current_user;
-      if !blank? === @user.email
-        # Tell the UserMailer to send a thank you Email after user creats a pin
-        UserMailer.thanks_email(@user).deliver
-      end
+      UserMailer.thanks_email(@user).deliver
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
       render action: 'new'
@@ -45,18 +41,19 @@ class PinsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_pin
-      @pin = Pin.find(params[:id])
-    end
 
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_pin
+    @pin = Pin.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-   def pin_params
-      params.require(:pin).permit(:description, :image)
-    end
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def pin_params
+    params.require(:pin).permit(:description, :image)
+  end
 end

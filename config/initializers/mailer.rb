a6264ee2aa3a105163bdcf6c744e367dbd5f2691
Sahil Dhankhar <sahil1345@gmail.com>
2014-08-29 16:56:57 +0530

    Correct Eamail

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.gmail.com',
   :port => 587,
-  :domain => "local.drop.me:300",
-  :user_name => "knowledgedropsat@gmail.com",
-  :password => "dropsbysahil",
+  :domain => "omr-pinteresting18.herokuapp.com",
+  :user_name => "davidbedussa21@gmail.com",
+  :password => "11092014",
   :authentication => "plain",
   :enable_starttls_auto => true
 }

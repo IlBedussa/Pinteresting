004a225e3ccc47382fc32e6522c4c0c4f5769f6d
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-10 21:34:56 +0530

    try

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,13 +7,20 @@ class Pin < ActiveRecord::Base
 	# validates :description, presence: true
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   before_save :add_defaults
+  default_scope where(:user_id.not_eq => nil)
   MESSAGES = ["Wish you a very happy birthday David!", "Many Many happy returns of the day David!", "Have a great Birthday David!", "Here’s to lots of celebration today! We wish you lots of good health for you and your loved ones in the coming year :)", "Remember this wise saying on your happy day: the more you celebrate your life, the more there is in life to celebrate!", "Have a wonderful happy, healthy birthday and many more to come. Happy Birthday!"]
   
   def add_defaults
     puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
     self.description = MESSAGES.sample if self.description.blank?
     puts "###########################################"
-    self.image = open "http://www.iam21.today/default_images/#{(1..24).to_a.sample}.jpg"  if self.image.blank?
+    self.image = open "http://www.iam21.today/default_images/#{(1..32).to_a.sample}.jpg"  if self.image.blank?
     puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  end
+  
+  def self.create_defaults
+    (1..32).to_a.each do |i|
+      Pin.create
+    end
   end
 end

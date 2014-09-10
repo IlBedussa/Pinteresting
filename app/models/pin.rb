1c4028fcd6ac1d14e976fb1bad5d33c38af1bfeb
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-10 20:14:58 +0530

    New Images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_many :likes, :dependent => :destroy
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	# validates :image, presence: true
-	validates :description, presence: true
+	# validates :description, presence: true
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   after_create :add_defaults
   MESSAGES = ["Wish you a very happy birthday David!", "Many Many happy returns of the day David!", "Have a great Birthday David!", "Here’s to lots of celebration today! We wish you lots of good health for you and your loved ones in the coming year :)", "Remember this wise saying on your happy day: the more you celebrate your life, the more there is in life to celebrate!", "Have a wonderful happy, healthy birthday and many more to come. Happy Birthday!"]
@@ -14,7 +15,7 @@ class Pin < ActiveRecord::Base
   
   def add_defaults
     self.description = MESSAGES.sample if self.description.blank?
-    self.image = open(IMAGES.sample) if self.image.blank?
+    self.image = open "http://www.1am21.today/assets/default_images/#{(1..24).to_a.sample}.jpg"  if self.image.blank?
     self.save!
   end
 end

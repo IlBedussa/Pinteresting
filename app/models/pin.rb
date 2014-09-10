03d8d108fd73d926d8f4121529966a07d8e2fef2
Sahil Dhankhar <sahil1345@gmail.com>
2014-09-10 20:54:49 +0530

    Logs

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,7 +10,10 @@ class Pin < ActiveRecord::Base
   MESSAGES = ["Wish you a very happy birthday David!", "Many Many happy returns of the day David!", "Have a great Birthday David!", "Here’s to lots of celebration today! We wish you lots of good health for you and your loved ones in the coming year :)", "Remember this wise saying on your happy day: the more you celebrate your life, the more there is in life to celebrate!", "Have a wonderful happy, healthy birthday and many more to come. Happy Birthday!"]
   
   def add_defaults
+    puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
     self.description = MESSAGES.sample if self.description.blank?
+    puts "###########################################"
     self.image = open "http://www.iam21.today/default_images/#{(1..24).to_a.sample}.jpg"  if self.image.blank?
+    puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   end
 end
